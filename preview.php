ff7c21b70f8b6d2fb54e0958e10bf7d416da1d2f
Emmanuel Chekumbe <emcie4@gmail.com>
2023-01-03 21:46:30 +0300

    Update preview.php

--- preview.php
+++ preview.php
@@ -1,51 +1,54 @@
 <?php
 // "preview.php" file
 
-$old_year = $_POST['old_year'];
+// Get the old year from the form submission
+$old_year = intval($_POST['old_year']);
+
+// Get the current year
 $new_year = date('Y');
 
-$count_posts = wp_count_posts();
-$published_posts = $count_posts->publish;
+// Get an array of posts that have not been updated
+$posts = get_posts(array(
+  'numberposts' => -1,
+  'post_type'   => 'post',
+  'meta_key'    => '_year_updater_updated',
+  'meta_value'  => 0
+));
 
-$updated_posts = 0;
-$progress = 0;
-?>
-
-<div class="progress-container">
-  <div id="progress-bar"></div>
-</div>
-
-<h2>Preview of updated titles:</h2>
-<ul>
-<?php
-$args = array(
-  'post_type' => 'post',
-  'post_status' => 'publish',
-  'posts_per_page' => -1,
-  's' => $old_year,
-);
-$posts = new WP_Query($args);
-if ($posts->have_posts()) {
-  while ($posts->have_posts()) {
-    $posts->the_post();
-    $title = get_the_title();
-    $updated_title = str_replace($old_year, $new_year, $title);
-    echo '<li>' . $updated_title . '</li>';
-    $updated_posts++;
-    $progress = ($updated_posts / $published_posts) * 100;
-    ?>
-    <script>
-      document.getElementById("progress-bar").style.width = "<?php echo $progress ?>%";
-      document.getElementById("progress-bar").innerHTML = "<?php echo $progress ?>%";
-    </script>
-    <?php
-  }
+// Build an array of the old and new titles for each post
+foreach ($posts as $post) {
+  $title = $post->post_title;
+  $updated_title = str_replace($old_year, $new_year, $title);
+  $post_updates[$post->ID] = array(
+    'old_title' => $title,
+    'new_title' => $updated_title
+  );
 }
 ?>
-</ul>
 
-<form method="post" action="admin.php?page=year-updater&step=update">
-  <input type="hidden" name="old_year" value="<?php echo $old_year ?>">
-  <input type="hidden" name="new_year" value="<?php echo $new_year ?>">
-  <input type="submit" name="submit" value="Change Year">
-</form>
+<div class="wrap">
+  <h1>Year Updater</h1>
+  <p>Here is a preview of the updated post titles:</p>
+  <table class="widefat">
+    <thead>
+      <tr>
+        <th>Old Title</th>
+        <th>New Title</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($post_updates as $post_id => $update): ?>
+      <tr>
+        <td><?php echo esc_html($update['old_title']); ?></td>
+        <td><?php echo esc_html($update['new_title']); ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+  <form method="post" action="admin.php?page=year-updater&step=update">
+    <?php wp_nonce_field('year_updater_update'); ?>
+    <input type="hidden" name="old_year" value="<?php echo esc_attr($old_year); ?>">
+    <input type="hidden" name="new_year" value="<?php echo esc_attr($new_year); ?>">
+    <input type="submit" name="submit" value="Update Year">
+  </form>
+</div>
