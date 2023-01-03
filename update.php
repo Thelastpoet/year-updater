420966b13e9833ae55db277dac19122fa951b293
Emmanuel Chekumbe <emcie4@gmail.com>
2023-01-03 22:06:58 +0300

    Update update.php

--- update.php
+++ update.php
@@ -5,30 +5,34 @@
 $old_year = intval($_POST['old_year']);
 $new_year = intval($_POST['new_year']);
 
-// Get an array of posts that have not been updated
+// Get an array of published posts that contain the old year in the title
 $posts = get_posts(array(
   'numberposts' => -1,
   'post_type'   => 'post',
-  'meta_key'    => '_year_updater_updated',
-  'meta_value'  => 0
+  'post_status' => 'publish',
+  's'           => $old_year
 ));
 
 // Update the year in the title and modified date for each post
 foreach ($posts as $post) {
   $post_id = $post->ID;
   $title = $post->post_title;
-  $modified_date = date('Y-m-d H:i:s');
   $updated_title = str_replace($old_year, $new_year, $title);
   wp_update_post(array(
-    'ID'           => $post_id,
-    'post_title'   => $updated_title,
-    'post_modified' => $modified_date
+    'ID'         => $post_id,
+    'post_title' => $updated_title,
   ));
-  update_post_meta($post_id, '_year_updater_updated', 1);
-}
-?>
-
-<div class="wrap">
-  <h1>Year Updater</h1>
-  <p>The year has been successfully updated in all published post titles!</p>
-</div>
+  $current_date = date('Y-m-d H:i:s');
+  wp_update_post(array(
+    'ID'                => $post_id,
+    'post_modified'     => $current_date,
+    'post_modified_gmt' => $current_date
+  ));
+    // Update the "year_updated" postmeta field with the new year
+    update_post_meta($post_id, 'year_updated', $new_year);
+  }
+  
+  echo '<div class="wrap">';
+  echo '<h1>Year Updater</h1>';
+  echo '<h2>Year updated successfully!</h2>';
+  echo '</div>';
