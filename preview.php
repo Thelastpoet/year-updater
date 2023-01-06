ef02d6e8df180e9553c602b6d45d92cd2f6b9abc
Emmanuel Chekumbe <emcie4@gmail.com>
2023-01-06 12:55:02 +0300

    Escaped all echo'd variables and html

--- preview.php
+++ preview.php
@@ -17,20 +17,35 @@ $posts = get_posts(array(
 
 // Display a preview of the updated titles
 echo '<div class="wrap">';
-echo '<h1>Year Updater</h1>';
-echo '<h2>Preview of updated titles:</h2>';
-echo '<ul>';
+echo '<h1>' . esc_html__('Year Updater', 'year-updater') . '</h1>';
+echo '<h2>' . esc_html__('Preview of updated titles:', 'year-updater') . '</h2>';
+echo '<table class="wp-list-table widefat striped">';
+echo '<thead>';
+echo '<tr>';
+echo '<th>' . esc_html__('Post Title', 'year-updater') . '</th>';
+echo '<th>' . esc_html__('Post ID', 'year-updater') . '</th>';
+echo '<th>' . esc_html__('Post Type', 'year-updater') . '</th>';
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
 foreach ($posts as $post) {
   $title = $post->post_title;
   $updated_title = str_replace($old_year, $new_year, $title);
-  echo '<li>' . $updated_title . '</li>';
+  $post_id = $post->ID;
+  $post_type = $post->post_type;
+  echo '<tr>';
+  echo '<td>' . esc_html($updated_title) . '</td>';
+  echo '<td>' . esc_html($post_id) . '</td>';
+  echo '<td>' . esc_html($post_type) . '</td>';
+  echo '</tr>';
 }
-echo '</ul>';
-echo '</div>';
+echo '</tbody>';
+echo '</table>';
 
 // Display the update form
 echo '<form method="post" action="admin.php?page=year-updater&step=update">';
-echo '<input type="hidden" name="old_year" value="' . $old_year . '">';
-echo '<input type="hidden" name="new_year" value="' . $new_year . '">';
-echo '<input type="submit" name="submit" value="Change Year">';
+echo '<input type="hidden" name="old_year" value="' . esc_attr($old_year) . '">';
+echo '<input type="hidden" name="new_year" value="' . esc_attr($new_year) . '">';
+echo '<input type="submit" name="submit" value="' . esc_attr__('Change Year', 'year-updater') . '">';
 echo '</form>';
+echo '</div>';
