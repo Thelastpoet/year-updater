fef741e5e98b3145ef380c4135d246a1df3ee5b1
thelastpoet <emcie4@gmail.com>
2024-02-19 10:30:44 +0300

    Introduced batch processing

--- includes/yu-process.php
+++ includes/yu-process.php
@@ -2,7 +2,7 @@
 
 namespace YearUpdater;
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
@@ -10,43 +10,53 @@ class YU_Process {
     private $year;
 
     public function __construct() {
-        $this->year = date( 'Y' );
+        $this->year = date('Y');
     }
 
-    public function update_year( $post_type ) {
+    public function update_year($post_type) {
         $new_year = $this->year;
-    
-        $args = [
-            'post_type'      => $post_type,
-            'post_status'    => 'publish',
-            'posts_per_page' => -1,
-        ];
-    
-        $query = new \WP_Query( $args );
-    
-        if ( ! $query->have_posts() ) {
-            return new \WP_Error( 'no_posts', __( 'No posts found to update.', 'year-updater' ) );
-        }
-        
-        foreach ( $query->posts as $post ) {
-            $post_id = $post->ID;
-            $title = $post->post_title;
 
-            if ( preg_match( '/\b20\d\d\b/', $title, $matches ) && $matches[0] !== $new_year ) {
-                $updated_title = preg_replace( '/\b20\d\d\b/', $new_year, $title );
+        // Batch processing
+        $posts_per_page = 20; 
+        $paged = 1;
 
-                wp_update_post( [
-                    'ID'         => $post_id,
-                    'post_title' => $updated_title,
-                ] );
+        do {
+            $args = [
+                'post_type'      => $post_type,
+                'post_status'    => 'publish',
+                'posts_per_page' => $posts_per_page,
+                'paged'          => $paged,
+                'no_found_rows'  => true, 
+            ];
 
-                // Store a custom field to indicate the post title was updated with a new year.
-                update_post_meta( $post_id, 'year_updated', $new_year );
+            $query = new \WP_Query($args);
+
+            if (!$query->have_posts()) {
+                break; // Exit the loop if no posts are found.
             }
-        }
 
-        wp_reset_postdata();
-    
+            foreach ($query->posts as $post) {
+                $post_id = $post->ID;
+                $title = $post->post_title;
+
+                if (preg_match('/\b20\d\d\b/', $title, $matches) && $matches[0] !== $new_year) {
+                    $updated_title = preg_replace('/\b20\d\d\b/', $new_year, $title);
+
+                    wp_update_post([
+                        'ID'         => $post_id,
+                        'post_title' => $updated_title,
+                    ]);
+
+                    // Store a custom field to indicate the post title was updated with a new year.
+                    update_post_meta($post_id, 'year_updated', $new_year);
+                }
+            }
+
+            wp_reset_postdata();
+            $paged++;
+
+        } while (true);
+
         return true;
     }
 }
